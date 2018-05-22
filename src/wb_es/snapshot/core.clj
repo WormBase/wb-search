473c5b73358829ffaadfa10a4c1a81aaec0aef83
Sibyl <sibyl@wormbase.org>
2018-05-22 14:36:20 +0000

    fix get-next-snapshot-id

--- src/wb_es/snapshot/core.clj
+++ src/wb_es/snapshot/core.clj
@@ -38,9 +38,13 @@
   [repository-name release-id]
   (let [pattern (re-pattern (format "snapshot_%s(_v(\\d+))?" release-id))
         current-id (get-lateset-snapshot-id repository-name release-id)
-        [_ _ version-id] (re-matches pattern current-id)]
-    (format "snapshot_%s_v%s" release-id (or (+ 1 (Integer. version-id))
-                                             0))))
+        next-version-id (if current-id
+                          (->> (re-matches pattern current-id)
+                               (last)
+                               (Integer.)
+                               (+ 1))
+                          0)]
+    (format "snapshot_%s_v%s" release-id next-version-id)))
 
 (defn save-snapshot
   [index-id repository-name snapshot-id]
