909209d34ad0b14a3c576e63444a2c9305d67666
Sibyl <sibyl@wormbase.org>
2019-03-04 16:34:30 +0000

    Version 1.0.0-alpha4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wb-es "1.0.0-alpha3"
+(defproject wb-es "1.0.0-alpha4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.7.0"
