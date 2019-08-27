cd7238d108e788cf3fb7580833379ea776dd624b
Sibyl <sibyl@wormbase.org>
2019-08-27 21:21:04 +0000

    use role instead of access key for Production environmnet but allow access key to be used for development

--- docker/es-entrypoint.sh
+++ docker/es-entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 set -e
 
-bin/elasticsearch-keystore create
+# If access key isn't provided, defaults to instance profile/role.
+# Access key is probably only useful if developing locally, ie not on AWS ec2
+if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_KEY}" ]; then
 
-# credentials for AWS S3 repository plugin
-echo ${AWS_ACCESS_KEY_ID} | bin/elasticsearch-keystore add --stdin s3.client.default.access_key
-echo ${AWS_SECRET_KEY} | bin/elasticsearch-keystore add --stdin s3.client.default.secret_key
+    bin/elasticsearch-keystore create
 
-# credentials for AWS EC2 discovery plugin
-echo ${AWS_ACCESS_KEY_ID} | bin/elasticsearch-keystore add --stdin discovery.ec2.access_key
-echo ${AWS_SECRET_KEY} | bin/elasticsearch-keystore add --stdin discovery.ec2.secret_key
+    # credentials for AWS S3 repository plugin
+    echo ${AWS_ACCESS_KEY_ID} | bin/elasticsearch-keystore add --stdin s3.client.default.access_key
+    echo ${AWS_SECRET_KEY} | bin/elasticsearch-keystore add --stdin s3.client.default.secret_key
+
+    # credentials for AWS EC2 discovery plugin
+    echo ${AWS_ACCESS_KEY_ID} | bin/elasticsearch-keystore add --stdin discovery.ec2.access_key
+    echo ${AWS_SECRET_KEY} | bin/elasticsearch-keystore add --stdin discovery.ec2.secret_key
+
+fi
 
 # sudo su
 # ulimit -n 65536
